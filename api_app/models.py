b70a9fad5e040bb5b8dbadc30e893634a7d50f95
Ugyen Jigten Dorji <ujigten@gmail.com>
2020-07-04 15:05:15 +0700

    Create models for Students and Schools

--- api_app/models.py
+++ api_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+class Student(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    student_id = models.CharField(max_length=20)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name + self.last_name
+
+class School(models.Model):
+    name = models.CharField(max_length=20)
+    max_students = 10
+
+    def __str__(self):
+        return self.name
