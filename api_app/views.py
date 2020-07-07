fe5a1c80bfdfa77f978fbb86d97dd43faa273a59
Ugyen Jigten Dorji <ujigten@gmail.com>
2020-07-07 15:17:13 +0700

    Added ordering of schools and students

--- api_app/views.py
+++ api_app/views.py
@@ -1,28 +1,31 @@
 from .serializers import SchoolSerializer, StudentSerializer, SchoolStudentSerializer
 from .models import School, Student
 from rest_framework import viewsets
-from rest_framework import filters
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 # Viewsets
 class SchoolViewSet(viewsets.ModelViewSet):
     serializer_class = SchoolSerializer
     queryset = School.objects.all()
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['name',]
+    ordering_fields = ['name',]
 
 class StudentViewSet(viewsets.ModelViewSet):
     serializer_class = StudentSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['first_name', 'last_name', '=student_id']
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ['first_name', 'last_name', '=student_id', 'birth_date']
+    ordering_fields = ['first_name', 'last_name', 'student_id', 'birth_date']
 
     def get_queryset(self):
         return Student.objects.all()
     
 class SchoolStudentViewSet(viewsets.ModelViewSet):
     serializer_class = SchoolStudentSerializer
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['first_name', 'last_name', '=student_id']
-
+    ordering_fields = ['first_name', 'last_name', 'student_id', 'birth_date']
+    
     def get_queryset(self):
         return Student.objects.filter(school=self.kwargs['school_pk'])
 
