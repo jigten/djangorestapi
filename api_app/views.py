afa9898f8fae997f3a692790cc8726dcb79165d2
Ugyen Jigten Dorji <ujigten@gmail.com>
2020-07-07 10:31:01 +0700

    Implement student searching by name and student ID

--- api_app/views.py
+++ api_app/views.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponse
 from .serializers import SchoolSerializer, StudentSerializer, SchoolStudentSerializer
 from .models import School, Student
 from rest_framework import viewsets
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
+from rest_framework import filters
 
 # Viewsets
 class SchoolViewSet(viewsets.ModelViewSet):
@@ -13,50 +10,15 @@ class SchoolViewSet(viewsets.ModelViewSet):
 
 class StudentViewSet(viewsets.ModelViewSet):
     serializer_class = StudentSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['first_name', 'last_name', '=student_id']
 
     def get_queryset(self):
         return Student.objects.all()
-
-    # override create method for maximum students check
-    # def create(self, request):
-    #     serializer_class = StudentSerializer
-    #     serializer = serializer_class(data=request.data)
-
-    #     if serializer.is_valid():
-    #         # get the relevant school
-    #         school_pk = serializer.validated_data.get("school").id
-    #         school = get_object_or_404(School, pk=school_pk)
-    #         # check if school has maximum number of students enrolled
-    #         if(school.student.count() == school.max_students):
-    #             # return error message if max students reached
-    #             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    #         else:
-    #             # enroll the student otherwise
-    #             serializer.save()
-    #             return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class SchoolStudentViewSet(viewsets.ModelViewSet):
     serializer_class = SchoolStudentSerializer
 
     def get_queryset(self):
         return Student.objects.filter(school=self.kwargs['school_pk'])
-
-    # override create method for maximum students check
-    # def create(self, request, school_pk=None):
-    #     serializer_class = StudentSerializer
-    #     serializer = serializer_class(data=request.data)
-
-    #     if serializer.is_valid():
-    #         # get the relevant school
-    #         school = get_object_or_404(School, pk=school_pk)
-    #         # check if school has maximum number of students enrolled
-    #         if(school.student.count() == school.max_students):
-    #             # return error message if max students reached
-    #             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    #         else:
-    #             # enroll the student otherwise
-    #             serializer.save(school=school)
-    #             return Response(serializer.data, status=status.HTTP_201_CREATED)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
