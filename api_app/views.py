cd14f63648c02ab8f0d2e08fe347f6f22e28e61d
Ugyen Jigten Dorji <ujigten@gmail.com>
2020-07-09 13:51:19 +0700

    Added Pagination for both schools and students

--- api_app/views.py
+++ api_app/views.py
@@ -2,10 +2,23 @@ from .serializers import SchoolSerializer, StudentSerializer, SchoolStudentSeria
 from .models import School, Student
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
+
+# Pagination classes
+class SchoolResultsSetPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 50
+
+class StudentResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 50
 
 # Viewsets
 class SchoolViewSet(viewsets.ModelViewSet):
     serializer_class = SchoolSerializer
+    pagination_class = SchoolResultsSetPagination
     queryset = School.objects.all()
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['name',]
@@ -13,6 +26,7 @@ class SchoolViewSet(viewsets.ModelViewSet):
 
 class StudentViewSet(viewsets.ModelViewSet):
     serializer_class = StudentSerializer
+    pagination_class = StudentResultsSetPagination
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['first_name', 'last_name', '=student_id', 'birth_date']
     ordering_fields = ['first_name', 'last_name', 'student_id', 'birth_date']
@@ -22,6 +36,7 @@ class StudentViewSet(viewsets.ModelViewSet):
     
 class SchoolStudentViewSet(viewsets.ModelViewSet):
     serializer_class = SchoolStudentSerializer
+    pagination_class = StudentResultsSetPagination
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ['first_name', 'last_name', '=student_id']
     ordering_fields = ['first_name', 'last_name', 'student_id', 'birth_date']
